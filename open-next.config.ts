6d3f1ab0323170bf64f42bc584146722d862285d
Samarth <programminglovers97@gmail.com>
2026-07-26 02:58:42 +0530

    Fix open-next.config.ts export syntax

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,2 +1,7 @@
-import defaultCloudflareConfig from "@opennextjs/cloudflare";
-export default defaultCloudflareConfig;
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next";
+
+const config = {
+  default: {},
+} satisfies OpenNextConfig;
+
+export default config;
